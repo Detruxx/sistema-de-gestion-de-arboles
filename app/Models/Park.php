3ab0a678114532bc61af32138368e7df9aefe3d8
OrnellaSaporito <ornesaporito@yahoo.com.ar>
2026-06-12 16:49:56 -0300

    Agregado de elementos para que anden los factories/seeders

--- app/Models/Park.php
+++ app/Models/Park.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Park extends Model
 {
-    //
+    use HasFactory;
 }
--- app/Models/Tree.php
+++ app/Models/Tree.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tree extends Model
 {
-    //
+    use HasFactory;
 
     protected $casts = [
         'vitality' => 'array', // Cada vez que guarda/lea este campo, lo conviérte automáticamente en un Array de PHP
