c5f5606b81ae89f48144e0e215cf7d76b86a0a68
OrnellaSaporito <ornesaporito@yahoo.com.ar>
2026-06-12 16:54:07 -0300

    Update SpecieSeeder.php

--- database/seeders/SpecieSeeder.php
+++ database/seeders/SpecieSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Specie;
 
-class SpeciesSeeder extends Seeder
+class SpecieSeeder extends Seeder
 {
     /**
      * Run the database seeds.
