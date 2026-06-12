bfc926593d7130a6074a699c1031425a1d3ffd05
OrnellaSaporito <ornesaporito@yahoo.com.ar>
2026-06-12 16:59:18 -0300

    Cambios nombres modelos de español a ingles

--- app/Models/Planter.php
+++ app/Models/Planter.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Plantera extends Model
+class Planter extends Model
 {
     use HasFactory;
 }
