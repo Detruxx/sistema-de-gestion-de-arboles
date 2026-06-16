ca1beb2e301d38b97b2ffa20c931fd0240b1be90
OrnellaSaporito <ornesaporito@yahoo.com.ar>
2026-06-16 00:17:10 -0300

    Update RequestTypeSeeder.php

--- database/seeders/RequestTypeSeeder.php
+++ database/seeders/RequestTypeSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Request_type;
+use Illuminate\Support\Facades\DB;
 
 class RequestTypeSeeder extends Seeder
 {
