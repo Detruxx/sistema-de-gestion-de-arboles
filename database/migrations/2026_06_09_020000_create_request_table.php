a2c8de6ff86216f288af461220602723f7e25471
OrnellaSaporito <ornesaporito@yahoo.com.ar>
2026-06-16 00:42:17 -0300

    Agregado tema imagenes en bdd

--- database/migrations/2026_06_09_020000_create_request_table.php
+++ database/migrations/2026_06_09_020000_create_request_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->foreignId('request_type_id')->constrained('request_types')->onDelete('restrict');
             $table->foreignId('street_id')->constrained('streets')->onDelete('restrict');
             $table->text('description');
+            $table->string('path'); // path que lleva a la foto
             $table->enum('status', ['open', 'in_progress', 'resolved'])->default('open');
             $table->timestamps();
         });
