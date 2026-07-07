55ea5a1bf21840c887a14c09dde974d663181a6e
OrnellaSaporito <ornesaporito@yahoo.com.ar>
2026-07-06 21:32:15 -0300

    algoritmo de scoring
    *Cosas hechas hoy*
    
    A pedido de tomi respecto a tarea 1:
    -Se actuaalizó el seeder de PrioritySeeder
    -Se actualizó la tabla de Priorities con el campo slug
    -Se le agrego este campo al modelo de Priority
    +Me falta sumar lo del RequestController

--- app/Models/Priority.php
+++ app/Models/Priority.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Priority extends Model
 {
-    protected $fillable = ['priority_name'];
+    protected $fillable = ['priority_name', 'slug',];
 
     // Una prioridad puede pertenecer a muchos reclamos
     public function requests()
